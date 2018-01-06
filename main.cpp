07c05046d1e3c0edadd17cfaa561b62e20cc805a
Sq <574146616@qq.com>
2018-01-07 02:08:13 +0800

    修改了系统初始化的时间，进程初始化的时间，让他们都从1开始；修改了时间片轮转的小bug；添加了高响应比优先算法

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ typedef struct PCB
     int need_time;          //进程需要运行的时间
     int cpu_time;           //进程已经占用CPU的时间
     process_state state;    //进程的状态
-    PCB *next;              //下一个进程的指针
+    double R;               //用于高响应比算法
 } PCB;
 
 int n;                          //进程的个数
@@ -64,15 +64,17 @@ void process_infor()
 {
     printf("\n\n\n");
     printf("%5s%8s%10s%10s%10s\n", "PID", "优先级", "进入时间", "运行时间", "需要时间");
+    //printf("%5s%8s%10s%10s\n", "PID", "优先级", "进入时间", "需要时间");
 
     for(int i = 0; i < n; i++)
     {
         PCB *process = PCB_array[i];
         printf("%5d%7d%8d%10d%10d\n", process->PID, process->priority, process->process_reach_time, process->cpu_time, process->need_time);
+        //printf("%5d%7d%8d%10d\n", process->PID, process->priority, process->process_reach_time, process->need_time);
     }
 }
 
-//用于先到先服务排序
+//用于先到先服务算法排序
 int FSFC_cmp(PCB *a, PCB *b)
 {
     return a->process_reach_time < b->process_reach_time;
@@ -88,12 +90,11 @@ void FCFS()
     sort(PCB_array, PCB_array + n, FSFC_cmp);
 
     //初始化时间
-    int time = 0;
+    int time = 1;
 
     //完成的进程数
     int process_finsih_count = 0;
-    //一个进程开始的时间
-    //int start_time = 0;
+
     for(; process_finsih_count < n; time++)
     {
         PCB *tmp = PCB_array[process_finsih_count];
@@ -101,8 +102,6 @@ void FCFS()
         //当进程到达的时间小于当前的时间并且进程的状态为‘READY’时
         if(tmp->process_reach_time <= time && tmp->state == READY)
         {
-            //记录进程开始时间（基本都不是进程到达的时间）
-            //start_time = time;
             //修改进程的状态为运行
             tmp->state = RUNNING;
 
@@ -130,6 +129,7 @@ void FCFS()
     }
 }
 
+//用于短进程优先（非抢占）算法排序
 int SPN_tmp_non_preemptive(PCB *a, PCB *b)
 {
     return a->need_time < b->need_time;
@@ -145,7 +145,7 @@ void SPN_non_preemptive()
     sort(PCB_array, PCB_array + n, FSFC_cmp);
 
     //初始化时间
-    int time = 0;
+    int time = 1;
     //已进入的进程个数
     int reach_process_num = 0;
     //完成的进程数
@@ -218,6 +218,7 @@ void SPN_non_preemptive()
     }
 }
 
+//用于短进程优先（抢占）算法排序
 int SPN_tmp_preemptive(PCB *a, PCB *b)
 {
     return a->need_time < b->need_time;
@@ -233,7 +234,7 @@ void SPN_preemptive()
     sort(PCB_array, PCB_array + n, FSFC_cmp);
 
     //初始化时间
-    int time = 0;
+    int time = 1;
     //已进入的进程个数
     int reach_process_num = 0;
     //完成的进程数
@@ -308,7 +309,7 @@ void RR()
     sort(PCB_array, PCB_array + n, FSFC_cmp);
 
     //初始化时间
-    int time = 0;
+    int time = 1;
     //时间片
     int time_slice = 5;
     //时间片计数
@@ -334,6 +335,7 @@ void RR()
                 if(temp->process_reach_time <= time)
                 {
                     reach_process_num ++;
+                    //将进程加入队尾
                     pcb_queue.push(temp);
                 }
                 else if(temp->process_reach_time > time)
@@ -349,59 +351,59 @@ void RR()
             PCB *now = pcb_queue.front();
             pcb_queue.pop();
 
-            if(now->cpu_time < now->need_time)
+            if(now->state == READY)
             {
-                if(now->state == READY)
-                {
-                	if(first)
-                	{
-	                	time++;
-	                }
-                    display(time, now);
-                    first = 1;
+            	if(first)
+            	{
+                	time++;
                 }
-
-                now->state = RUNNING;
-
-                time_slice_count = 0;
-
-                while(time_slice_count < time_slice && now->cpu_time < now->need_time)
-                {
-                    time ++;
-                    time_slice_count ++;
-                    now->cpu_time ++;
-
-                    display(time, now);
-                }
-
-                //在当前进程加入队尾之前，检查是否有新的进程进入
-                for(int i = reach_process_num; i < n; i++)
-                {
-                    PCB *temp = PCB_array[i];
-
-                    if(temp->process_reach_time <= time)
-                    {
-                        reach_process_num ++;
-                        pcb_queue.push(temp);
-                    }
-                    else if(temp->process_reach_time > time)
-                    {
-                        break;
-                    }
-                }
-
-                if(now->cpu_time == now->need_time)
-                {
-                    now->state = FINISH;
-                    process_finsih_count ++;
-                }
-                else
-                {
-                    pcb_queue.push(now);
-                }
-
-                time_slice_count = 0;
+                display(time, now);
+                first = 1;
             }
+
+            now->state = RUNNING;
+
+            time_slice_count = 0;
+
+            //在时间片内执行进程
+            while(time_slice_count < time_slice && now->cpu_time < now->need_time)
+            {
+                time ++;
+                time_slice_count ++;
+                now->cpu_time ++;
+
+                display(time, now);
+            }
+
+            //在当前进程加入队尾之前，检查是否有新的进程进入
+            for(int i = reach_process_num; i < n; i++)
+            {
+                PCB *temp = PCB_array[i];
+
+                if(temp->process_reach_time <= time)
+                {
+                    reach_process_num ++;
+                    pcb_queue.push(temp);
+                }
+                else if(temp->process_reach_time > time)
+                {
+                    break;
+                }
+            }
+
+            //进程已完成
+            if(now->cpu_time == now->need_time)
+            {
+                now->state = FINISH;
+                process_finsih_count ++;
+            }
+            //进程还未完成
+            else
+            {
+                pcb_queue.push(now);
+            }
+
+            time_slice_count = 0;
         }
         else
         {
@@ -412,6 +414,7 @@ void RR()
     }
 }
 
+//用于动态优先级算法排序
 struct priority_cmp
 {
     bool operator()(PCB a, PCB b)
@@ -429,11 +432,12 @@ void PRIORITY()
     //按照进程进入时间进行排序
     sort(PCB_array, PCB_array + n, FSFC_cmp);
 
-    //创建以priority_cmp排序优先队列
+    //创建以priority_cmp排序的优先队列
+    //为了保存原数据，这里不使用指针
     priority_queue<PCB, vector<PCB>, priority_cmp> pq;
 
     //初始化时间
-    int time = 0;
+    int time = 1;
     //已进入的进程个数
     int reach_process_num = 0;
     //完成的进程数
@@ -477,7 +481,7 @@ void PRIORITY()
             else if(tmp->cpu_time < tmp->need_time && tmp->state == RUNNING)
             {
                 tmp->cpu_time ++;
-                tmp->priority = --tmp->priority >= 0 ? tmp->priority : 0;
+                tmp->priority = (tmp->priority -= 2) >= 0 ? tmp->priority : 0;
                 flag = 2;
 
                 display(time, tmp);
@@ -498,7 +502,7 @@ void PRIORITY()
                 while(!pq.empty())
                 {
                     PCB tmp1 = pq.top();
-                    tmp1.priority ++;
+                    tmp1.priority = tmp1.priority > 32 ? 32 : tmp1.priority;
                     q.push(tmp1);
                     pq.pop();
                 }
@@ -510,10 +514,120 @@ void PRIORITY()
                 }
             }
 
+            //当前进程未完成
             if(flag)
             {
                 //重新放入优先队列
                 pq.push(*tmp);
+            }
+        }
+        else
+        {
+            display(time, NULL);
+        }
+    }
+}
+
+//用于动态优先级算法排序
+struct HHRN_cmp
+{
+    bool operator()(PCB *a, PCB *b)
+    {
+        return a->R < b->R;
+    }
+};
+
+//高响应比优先
+void HHRN()
+{
+    //显示表头
+    table_head();
+
+    //按照进程进入时间进行排序
+    sort(PCB_array, PCB_array + n, FSFC_cmp);
+
+    //创建以HHRN_cmp排序的优先队列
+    priority_queue<PCB*, vector<PCB*>, HHRN_cmp> pq;
+
+    //初始化时间
+    int time = 1;
+    //已进入的进程个数
+    int reach_process_num = 0;
+    //完成的进程数
+    int process_finsih_count = 0;
+
+    for(; process_finsih_count < n; time++)
+    {
+        //每个时间片检查是否有新的进程进入
+        for(int i = reach_process_num; i < n; i++)
+        {
+            PCB *temp = PCB_array[i];
+
+            if(temp->process_reach_time == time)
+            {
+                reach_process_num ++;
+                pq.push(temp);
+            }
+            else if(temp->process_reach_time > time)
+            {
+                break;
+            }
+        }
+
+        //优先队列不为空时
+        if(!pq.empty())
+        {
+            //计算每个在就绪队列的进程的响应比
+            for(int i = process_finsih_count; i < reach_process_num; i++)
+            {
+                PCB *temp = PCB_array[i];
+                temp->R = (double)time / temp->need_time;
+            }
+
+            PCB *tmp = pq.top();
+            pq.pop();
+
+            int flag = 0;
+
+            if(tmp->state == READY)
+            {
+                tmp->state = RUNNING;
+
+                display(time, tmp);
+            }
+            else if(tmp->cpu_time < tmp->need_time && tmp->state == RUNNING)
+            {
+                tmp->cpu_time ++;
+
+                display(time, tmp);
+            }
+            else if(tmp->cpu_time == tmp->need_time && tmp->state == RUNNING)
+            {
+                flag = 1;
+                tmp->state = FINISH;
+                //结束时没有消耗时间
+                time --;
+
+                //当前执行的与process_finsih_count位置的PCB_array的进程交换位置，便于操作
+                for(int i = 0; i < n; i++)
+                {
+                    if(PCB_array[i] == tmp)
+                    {
+                        PCB_array[i] = PCB_array[process_finsih_count];
+                        PCB_array[process_finsih_count] = tmp;
+                        break;
+                    }
+                }
+
+                //完成进程数加+1
+                process_finsih_count ++;
+            }
+
+            //当前进程未完成
+            if(!flag)
+            {
+                //重新放入优先队列
+                pq.push(tmp);
             }
         }
         else
@@ -538,11 +652,12 @@ void init()
     for(int i = 0; i < n; i++)
     {
         PCB *tmp = PCB_array[i];
-        tmp->cpu_time = 0;
+        tmp->cpu_time = 1;
         tmp->state = READY;
     }
 }
 
+//释放创建的内存
 void free_pcb()
 {
     for(int i = 0; i < n; i++)
@@ -562,7 +677,8 @@ void menu()
     printf("\t3.短进程优先（抢占）\n");
     printf("\t4.时间片轮换\n");
     printf("\t5.动态优先级\n");
-    printf("\t6.退出\n");
+    printf("\t6.高响应比优先\n");
+    printf("\t7.退出\n");
     printf("请选择：");
 
     int cas;
@@ -590,6 +706,10 @@ void menu()
             break;
 
         case 6:
+            HHRN();
+            break;
+
+        case 7:
             free_pcb();
             exit(0);
 
@@ -611,48 +731,45 @@ int main()
 
     printf("输入进程数：");
     scanf("%d", &n);
-    for(int i = 0;i < n; i++)
-    {
-        PCB *tmp = (PCB*)malloc(sizeof(PCB));
-
-        printf("输入进程PID：");
-        scanf("%d", &(tmp->PID));
-
-        printf("输入进程优先级：");
-        scanf("%d", &(tmp->priority));
-
-        printf("输入进程进入时间：");
-        scanf("%d", &(tmp->process_reach_time));
-
-        printf("输入进程需要运行的时间：");
-        scanf("%d", &(tmp->need_time));
-
-        tmp->cpu_time = 0;
-        tmp->state = READY;
-        tmp->next = NULL;
-
-        PCB_array[i] = tmp;
-    }
-
-
-//    srand((unsigned)time(NULL));
-//    for(int i = 0; i < n; i++)
+//    for(int i = 0;i < n; i++)
 //    {
 //        PCB *tmp = (PCB*)malloc(sizeof(PCB));
 //
 //        tmp->PID = i + 1;
-//        //优先级在0-32
-//        tmp->priority = (rand() % (32 - 0 + 1)) + 0;
-//        //到达时间在1-20
-//        tmp->process_reach_time = (rand() % (20 - 1)) + 1;
-//        //需要运行时间1-30
-//        tmp->need_time = (rand() % (30 - 1)) + 1;
-//        tmp->cpu_time = 0;
+//
+//        printf("输入进程优先级：");
+//        scanf("%d", &(tmp->priority));
+//
+//        printf("输入进程进入时间：");
+//        scanf("%d", &(tmp->process_reach_time));
+//
+//        printf("输入进程需要运行的时间：");
+//        scanf("%d", &(tmp->need_time));
+//
+//        tmp->cpu_time = 1;
 //        tmp->state = READY;
-//        tmp->next = NULL;
 //
 //        PCB_array[i] = tmp;
 //    }
+
+
+    srand((unsigned)time(NULL));
+    for(int i = 0; i < n; i++)
+    {
+        PCB *tmp = (PCB*)malloc(sizeof(PCB));
+
+        tmp->PID = i + 1;
+        //优先级在0-32
+        tmp->priority = (rand() % (32 - 0 + 1)) + 0;
+        //到达时间在1-20
+        tmp->process_reach_time = (rand() % (20 - 1)) + 1;
+        //需要运行时间1-30
+        tmp->need_time = (rand() % (30 - 1)) + 1;
+        tmp->cpu_time = 1;
+        tmp->state = READY;
+
+        PCB_array[i] = tmp;
+    }
 
     menu();
 
